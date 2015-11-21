d2affa1673c71de2f4e4566dbc5e1c44ffa23260
Vik Paruchuri <vik.paruchuri@gmail.com>
2015-11-21 09:51:29 -0600

    Modify dockerfiles

--- run_container.sh
+++ run_container.sh
@@ -3,5 +3,5 @@
 NAME=$1
 FOLDER=$2
 
-CONTAINER=`docker run -d -p 8888:8888 -v $FOLDER:/home/ds/notebooks dataquestio/$NAME`
+CONTAINER=`docker run -d -p 8888:8888 -v $FOLDER:/home/ds/notebooks dataquestio/$NAME-starter`
 echo $CONTAINER
